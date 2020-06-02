29b84813b3a53b683a7c5ce61b2738467954ac96
Adam Critchley <adamc@cromulence.com>
2020-06-01 18:23:59 -0700

    Update to latest qemu-shoggoth

--- run_http_dataflow.sh
+++ run_http_dataflow.sh
@@ -1,1 +1,1 @@
-./src/qemu-shoggoth/x86_64-softmmu/qemu-system-x86_64 --drive file=debian9.qcow2,format=qcow2 -m 2G -snapshot -monitor stdio -plugin http_dataflow -device e1000,netdev=net0 -netdev user,id=net0,hostfwd=tcp::8888-:8000
+./qemu-system-x86_64 --drive file=debian9.qcow2,format=qcow2 -m 2G -snapshot -monitor stdio -plugin http_dataflow -device e1000,netdev=net0 -netdev user,id=net0,hostfwd=tcp::8888-:8000
