7590b581bf990bc97f73a8e9945ca7fad20fb212
Adam Critchley <shoggoth@cromulence.com>
2020-06-18 18:40:55 -0700

    Tweaked setup scripts and update qemu-shoggoth with qt gui fix

--- init.sh
+++ init.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-git submodule init
-git submodule update
-sudo apt install libtool-bin autoconf bison flex libpixman-1-dev zlib1g-dev libglib2.0-dev python3-dev libsdl2-dev python3-pip python3-setuptools qtdeclarative5-dev
+git submodule update --init
+sudo apt install libtool-bin autoconf bison flex libpixman-1-dev zlib1g-dev libglib2.0-dev python3-dev libsdl2-dev python3-pip python3-setuptools python-is-python3 qtdeclarative5-dev libcapstone-dev 
 ./download.sh
 cd qemu-shoggoth; ./configure --target-list="x86_64-softmmu" --enable-sdl --enable-debug
